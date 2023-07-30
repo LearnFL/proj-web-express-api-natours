b1c78e35c19fa2fc79213e04c0a8761f7253cf49
LearnFL <denys.rotnov@gmail.com>
2023-07-30 13:23:07 -0400

    Stripe fix success url

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -26,9 +26,7 @@ export default class BookingsController {
         // }&user=${req.user.id}&price=${tour.price}`,
 
         // NOTE FIXED BUG DESCRIBED ABOVE
-        success_url: `${req.protocol}://${req.get('host')}/my-tours/${
-          tour.slug
-        }`,
+        success_url: `${req.protocol}://${req.get('host')}/my-tours/`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId, // to create booking in DB
